0f894e694f0878ee0eb7d27046c1764f50c376a3
Truong Tuan Manh <tuanmanh.truong@vn.panasonic.com>
2016-10-19 19:02:01 +0000

    Task1 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "signup" => "homes#new"
+  get "signup" => "users#new"
+  post "signup" => "users#create"
   root "homes#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
